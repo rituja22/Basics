39ce4cae73520e6de23cd81f9ffe32c51db003fc
rituja22 <109826066+rituja22@users.noreply.github.com>
2022-09-02 19:55:29 +0530

    Add files via upload

---
+++ print n natural nos in reverse order using loop.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	int i;
+	printf("enter the value of n \n");
+	scanf("%d", &n);
+	for(i=n;i>0;i--)
+	{
+		printf("the values are %d \n",i);
+	}
+	return 0;
+}
